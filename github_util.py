9bfb3582f75da053537ea27aab4213e784d0ebd2
Tim O'Hearen <tohearen@stevens.edu>
2019-09-21 18:21:39 -0400

    create baseline implementation + test

---
+++ github_util.py
@@ -0,0 +1,20 @@
+from github import Github
+from prettytable import PrettyTable
+
+
+def get_repo_dict(user_id: str) -> dict:
+    client = Github()
+    user = client.get_user(user_id)
+    repo_dict = {}
+    for repo in user.get_repos():
+        repo_dict[repo.full_name] = repo.get_commits().totalCount
+    return repo_dict
+
+
+def print_pretty_repo_table(user_id: str, repo_dict: dict):
+    repo_table = PrettyTable()
+    repo_table.field_names = ["Repo Name", "Number of Commits"]
+    print("\nRepo Report For User '" + user_id + "'")
+    for repo_name in repo_dict:
+        repo_table.add_row([repo_name, repo_dict[repo_name]])
+    print(repo_table.get_string())
---
+++ test_github_util.py
@@ -0,0 +1,23 @@
+import pytest
+
+import github_util
+
+
+@pytest.mark.parametrize(
+    "user_id, expected_repos",
+    [
+        ("tohearen", {
+            "tohearen/2019F-SSW-567-WS": 1,
+            "tohearen/2019F-SSW-567-WS-HW-02a": 11,
+            "tohearen/2019F-SSW-567-WS-PYTHON": 6,
+            "tohearen/ssw555-tm06-17A": 100
+        })
+    ],
+)
+def test_get_repos(user_id, expected_repos):
+    actual_repos = github_util.get_repo_dict(user_id)
+
+    for repo_name in actual_repos:
+        assert actual_repos[repo_name] >= expected_repos[repo_name]
+
+    github_util.print_pretty_repo_table(user_id, actual_repos)
